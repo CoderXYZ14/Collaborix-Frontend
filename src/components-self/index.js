acdd1fc8f8d3995e8d0b8e1ae0bf6067052946a4
CoderXYZ14 <s1404islam@gmail.com>
2024-10-03 18:46:29 +0530

    Container component added

--- src/components-self/index.js
+++ src/components-self/index.js
@@ -1,4 +1,4 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
-
-export { Header, Footer };
+import Container from "./container/Container";
+export { Header, Footer, Container };
