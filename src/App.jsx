0c924aeabdf6c7a4a39729a62b8f5aef45a416a6
CoderXYZ14 <s1404islam@gmail.com>
2024-10-03 18:36:38 +0530

    Header outlet footer set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import { Footer, Header } from "./components-self";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
