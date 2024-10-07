79e94492f36fe388d6cb4606bf2b6cefe613c1d2
CoderXYZ14 <s1404islam@gmail.com>
2024-10-08 00:42:50 +0530

    Login from local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import "./App.css";
 import { Footer, Header } from "./components-self";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-import { login } from "./store/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("userData");
+
+    if (userData) {
+      const parsedData = JSON.parse(userData);
+      dispatch(login({ userData: parsedData }));
+      navigate("/");
+    }
+  }, [dispatch, navigate]);
+
   return (
     <div className="min-h-screen flex flex-wrap content-between">
       <div className="w-full block">
