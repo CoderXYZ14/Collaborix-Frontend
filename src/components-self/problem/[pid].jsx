bccb05c247268eb5156cd3fabdf38e26f62464bd
CoderXYZ14 <s1404islam@gmail.com>
2024-10-28 09:02:40 +0530

    Pid.jsx updated

--- src/components-self/problem/[pid].jsx
+++ src/components-self/problem/[pid].jsx
@@ -1,10 +1,18 @@
-import React from "react";
-import Codespace from "../codespace/Codespace"; // Importing Codespace component
+import { useParams } from "react-router-dom";
+import { problems } from "../utils/problems";
+import Codespace from "./codespace/Codespace";
 
 const ProblemPage = () => {
+  const { pid } = useParams();
+  const problem = problems[pid];
+
+  if (!problem) {
+    return <div>Problem not found</div>;
+  }
+
   return (
     <div>
-      <Codespace />
+      <Codespace problem={problem} pid={pid} />
     </div>
   );
 };
