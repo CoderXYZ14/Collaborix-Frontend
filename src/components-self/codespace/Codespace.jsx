b1cc8f43a5de9eedf40b7eadec524cef27d2ab90
CoderXYZ14 <s1404islam@gmail.com>
2024-10-31 07:43:41 +0530

    useWindowSize hook created

--- src/components-self/codespace/Codespace.jsx
+++ src/components-self/codespace/Codespace.jsx
@@ -1,12 +1,14 @@
 import Split from "react-split";
 import Playground from "./playground/Playground";
 import ProblemDescription from "./problem-description/ProblemDescription";
+import Confetti from "react-confetti/dist/types/Confetti";
 
 const Codespace = ({ problem }) => {
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription problem={problem} />
       <Playground problem={problem} />
+      <Confetti gravity={0.3} tweenDuration={3000} />
     </Split>
   );
 };
