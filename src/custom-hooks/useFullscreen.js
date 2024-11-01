c540246a0810296e93cbe5ed40a64b78caeb35be
CoderXYZ14 <s1404islam@gmail.com>
2024-11-02 00:39:00 +0530

    useFullScreen hook created

---
+++ src/custom-hooks/useFullscreen.js
@@ -0,0 +1,31 @@
+const { useEffect, useState, useCallback } = require("react");
+
+const useFullscreen = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFullscreen(document.fullscreenElement !== null);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    return () =>
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+  }, []);
+
+  const toggleFullscreen = useCallback(async () => {
+    try {
+      if (!document.fullscreenElement) {
+        await document.documentElement.requestFullscreen();
+      } else {
+        await document.exitFullscreen();
+      }
+    } catch (err) {
+      console.error("Error toggling fullscreen:", err);
+    }
+  }, []);
+
+  return {
+    isFullscreen,
+    toggleFullscreen,
+  };
+};
