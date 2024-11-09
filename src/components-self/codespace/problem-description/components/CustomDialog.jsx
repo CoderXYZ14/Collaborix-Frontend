23a9f8be4cc0ae20030737536dc99480d3a11e97
CoderXYZ14 <s1404islam@gmail.com>
2024-11-10 01:30:18 +0530

    customDialog added

--- src/components-self/codespace/problem-description/components/CustomDialog.jsx
+++ src/components-self/codespace/problem-description/components/CustomDialog.jsx
@@ -4,23 +4,50 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-const CustomDialog = ({ isOpen, onClose, onJoin, roomId, setRoomId }) => {
+const CustomDialog = ({
+  isOpen,
+  onClose,
+  title,
+  description,
+  inputPlaceholder,
+  inputValue,
+  onInputChange,
+  onConfirm,
+  confirmButtonText,
+  confirmButtonColor = "bg-purple-700 text-gray-300", // Default style
+}) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent>
+      <DialogContent className="sm:max-w-md bg-gradient-to-b from-violet-200 from-10% to-purple-100 dark:bg-gradient-to-b dark:from-slate-800 dark:from-5% dark:to-purple-900 transition-colors duration-200">
         <DialogHeader>
-          <DialogTitle>Join Room</DialogTitle>
+          <DialogTitle className="dark:text-gray-300">{title}</DialogTitle>
         </DialogHeader>
-        <div className="flex flex-col space-y-4">
-          <Input
-            placeholder="Enter room ID"
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-          />
-          <Button onClick={onJoin}>Join</Button>
+        <div className="space-y-4">
+          {description && (
+            <div className="text-sm text-slate-600 dark:text-slate-300">
+              {description}
+            </div>
+          )}
+          {inputPlaceholder && (
+            <div className="flex items-center space-x-2">
+              <Input
+                value={inputValue}
+                onChange={(e) => onInputChange(e.target.value)}
+                placeholder={inputPlaceholder}
+                className="font-mono text-sm dark:text-gray-200 text-gray-800"
+              />
+              <Button
+                size="sm"
+                className={confirmButtonColor}
+                onClick={onConfirm}
+              >
+                {confirmButtonText}
+              </Button>
+            </div>
+          )}
         </div>
       </DialogContent>
     </Dialog>
--- src/components-self/codespace/problem-description/components/index.js
+++ src/components-self/codespace/problem-description/components/index.js
@@ -1,3 +1,4 @@
 import CustomHoverCard from "./CustomHoverCard";
+import CustomDialog from "./CustomDialog";
 
-export { CustomHoverCard };
+export { CustomHoverCard, CustomDialog };
