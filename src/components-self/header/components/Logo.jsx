54b699d44d15a36e7a90e4b75d998584c78e6bf7
CoderXYZ14 <s1404islam@gmail.com>
2024-11-09 19:08:53 +0530

    ProblemNavigation and Logo component created for header

---
+++ src/components-self/header/components/Logo.jsx
@@ -0,0 +1,22 @@
+import { Code2 } from "lucide-react";
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="flex items-center flex-wrap space-x-3">
+      <div className="flex items-center justify-center w-8 h-8 rounded-lg bg-white/10">
+        <Code2 size={20} className="text-gray-100" />
+      </div>
+      <div>
+        <h1 className="text-base font-bold tracking-wide text-gray-100 font-mono">
+          Collaborix
+        </h1>
+        <p className="text-xs font-medium tracking-wide text-purple-300">
+          {"<Code /> Together, Build Better"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components-self/header/components/index.js
@@ -0,0 +1,4 @@
+import Logo from "./Logo";
+import ProblemNavigation from "./ProblemNavigation";
+
+export { Logo, ProblemNavigation };
