36aa379286a0f522bf6ff42b7a6375a740530ea0
CoderXYZ14 <s1404islam@gmail.com>
2024-11-09 21:17:31 +0530

    Removed footer

--- src/components-self/problem-set/ProblemSet.jsx
+++ src/components-self/problem-set/ProblemSet.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 import LoadingSkeletonProblemList from "../loading-skeleton/LoadingSkeletonProblemList.jsx";
 import { ProblemTableHeader, ProblemList } from "./components";
 
